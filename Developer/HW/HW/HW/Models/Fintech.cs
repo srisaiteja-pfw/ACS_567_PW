f6a88d294e520eed198904d8548d89ed3633bf6b
DESKTOP-3QVT0OT\Vruddhi <vrusikapre2@gmail.com>
2023-03-10 13:54:47 -0500

    Sprint 1: Create,Read,Update,Delete Account

---
+++ Developer/HW/HW/HW/Models/Fintech.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Runtime.CompilerServices;
+
+namespace HW.Models
+{
+    public class Fintech
+    {
+        /// <summary>
+		/// Added getters and setters for all the input var
+		/// </summary>
+        /// 
+        public int Id { get; set; }
+
+        public string Firstname { get; set; } = String.Empty;
+
+        public string Lastname { get; set; } = String.Empty;
+
+        public string Address { get; set; } = String.Empty;
+
+        public string Email { get; set; } = String.Empty;
+
+        public string Contact { get; set; } = String.Empty;
+
+        public int Age { get; set; }
+
+        public string SSN { get; set; } = String.Empty;
+
+        public double Balance { get; set; }
+
+    }
+}
