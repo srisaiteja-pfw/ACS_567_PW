7ebf75984fd66d20a9365f936fb12156c1b7b40f
DESKTOP-3QVT0OT\Vruddhi <vrusikapre2@gmail.com>
2023-03-24 15:10:40 -0400

    Sprint 2 changes

---
+++ Updates/HW/HW/HW/Interfaces/IFintechRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using HW.Models;
+namespace HW.Interfaces
+{
+    public interface IFintechRepository
+    {
+        ICollection<Fintech> getItems();
+
+        Fintech GetItem(int id);
+
+        bool AccountExists(int id);
+
+        bool CustomerService(int id);
+
+        bool AddAccount(Fintech account);
+
+        bool EditAccount(Fintech account);
+
+        bool DeleteAccount(int  id);
+
+        ICollection<FinTechModel> getAllExpenses();
+
+        FinTechModel GetExpense(int id);
+
+        bool AddExpense(FinTechModel expense);
+
+        bool UpdateExpense(FinTechModel expense);
+
+        bool DeleteExpense(int id);
+
+        Dictionary<string, dynamic> analyzeBill();
+
+        //DataAnalysis DataAnalysis();
+        bool Save();
+    }
+}
