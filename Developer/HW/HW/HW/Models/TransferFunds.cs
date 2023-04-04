9917f11133bd727155c794bab1590eeaa5e7f8ec
DESKTOP-3QVT0OT\Vruddhi <vrusikapre2@gmail.com>
2023-04-04 18:57:09 -0400

    Sprint 2

--- Developer/HW/HW/HW/Models/TransferFunds.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HW.Models
-{
-    public class TransferFunds
-    {
-        public int Id { get; set; }
-        public int Account_number { get; set; }
-        public int Amount { get; set; }
-    }
-}
